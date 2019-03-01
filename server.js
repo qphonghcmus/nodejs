04b710319517bc7ee0caadf5ad3864cf566cfc5a
Quoc Phong <1612498@student.hcmus.edu.vn>
2019-03-01 19:48:55 +0700

    456

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app = express();
 
 // setup port
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get("/", function(req, res) {
     res.writeHead(200, {"Content-Type":"text/plain"});
