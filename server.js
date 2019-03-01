5a5a8e91995e9316b40af72640c387da31553182
Quoc Phong <1612498@student.hcmus.edu.vn>
2019-03-01 19:23:50 +0700

    123

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get("/", function(req, res) {
-    res.end("Hello");
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.end("Demo nodejs");
 });
 
 app.listen(server_port,server_ip_address, function() {
